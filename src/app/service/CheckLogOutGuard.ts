ad9cce9fb3c74e08bea96c1322ec691e68a41d97
nguyendongminhtri <nguyendongminhtri29092014@gmail.com>
2024-03-16 05:36:23 +0700

    check log out

--- src/app/service/CheckLogOutGuard.ts
+++ src/app/service/CheckLogOutGuard.ts
@@ -1,17 +1,18 @@
-import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from "@angular/router";
-import {Observable} from "rxjs";
-import {TokenService} from "./token.service";
 import {Injectable} from "@angular/core";
+import {TokenService} from "./token.service";
+import {ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree} from "@angular/router";
+import {Observable} from "rxjs";
+
 @Injectable({
   providedIn: 'root'
 })
-export class CheckLoginGuard implements CanActivate{
+export class CheckLogOutGuard {
   constructor(private tokenService: TokenService,
               private router: Router) {
   }
   // @ts-ignore
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if (!this.tokenService.getToken()){
+    if (this.tokenService.getToken()){
       return true;
     } else {
       this.router.navigate([''])
